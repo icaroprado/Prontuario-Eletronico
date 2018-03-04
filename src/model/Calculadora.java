eaf9d7b53b7b8a6c39ccfd0d3ed591cc0970a986
icaro <icaro.prado@dcomp.ufs.br>
2018-03-04 16:43:06 -0300

    ultimas classes do model criadas, ainda faltam as implementações.

--- src/model/Calculadora.java
+++ src/model/Calculadora.java
@@ -5,5 +5,10 @@ public class Calculadora {
 	public float calculaIMC(float _peso,float _comprimento) {
 		return _peso/(_comprimento*_comprimento);
 	}
-	
+	public float calculaPercentil(float _peso) {
+		return _peso;
+	}
+	public void calculaEstatistica() {
+		
+	}
 }
